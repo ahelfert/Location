05ebce986118f056afae9ec7c9e4fde59f9d6b35
ahelfert <ahelfert@gmx.de>
2016-09-22 10:33:20 +0200

    1

--- app/src/main/java/de/example/andy/location/MainActivity.java
+++ app/src/main/java/de/example/andy/location/MainActivity.java
@@ -1,15 +1,13 @@
-package de.example.frank.location;
+package de.example.andy.location;
 
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -18,9 +16,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.security.Permissions;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
